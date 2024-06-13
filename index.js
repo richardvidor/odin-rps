55a32dd4948432b9f639722294946502af2c2b52
Richard Vidor <richardvidor1@gmail.com>
2024-06-13 10:40:18 +0100

    craete function to play one round

--- index.js
+++ index.js
@@ -15,9 +15,28 @@ let humanScore = 0;
 let computerScore = 0;
 
 const playRound = (humanChoice, computerChoice) => {
-
-
+    if ((humanChoice === 'rock' && computerChoice === 'scissors') ||
+        (humanChoice === 'paper' && computerChoice === 'rock') ||
+        (humanChoice === 'scissors' && computerChoice === 'paper')) {
+        console.log(`You chose ${humanChoice}, computer chose ${computerChoice}`);
+        console.log(`${humanChoice} beats ${computerChoice}`);
+        console.log('You win!!');
+        humanScore++;
+    } else if ((humanChoice === 'rock' && computerChoice === 'paper') ||
+        (humanChoice === 'paper' && computerChoice === 'scissors') ||
+        (humanChoice === 'scissors' && computerChoice === 'rock')) {
+        console.log(`You chose ${humanChoice}, computer chose ${computerChoice}`);
+        console.log(`${computerChoice} beats ${humanChoice}`);
+        console.log('You lose');
+        computerScore++;
+    } else {
+        console.log(`You chose ${humanChoice}, computer chose ${computerChoice}`);
+        console.log('Draw');
+    }
+    console.log(`Your score: ${humanScore}, Computer score: ${computerScore}`);
 }
 
-// getComputerChoice();
-// getHumanChoice();
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
