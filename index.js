df731c6afb22c2cbfadc7389e343fe2c05c3ca4d
Richard Vidor <richardvidor1@gmail.com>
2024-06-14 11:43:13 +0100

    create divs with event listeners to display scores and winners of each round

--- index.js
+++ index.js
@@ -14,27 +14,27 @@ const getComputerChoice = () => {
 let humanScore = 0;
 let computerScore = 0;
 
-const playRound = (humanChoice, computerChoice) => {
-    if ((humanChoice === 'rock' && computerChoice === 'scissors') ||
-        (humanChoice === 'paper' && computerChoice === 'rock') ||
-        (humanChoice === 'scissors' && computerChoice === 'paper')) {
-        console.log(`You chose ${humanChoice}, computer chose ${computerChoice}`);
-        console.log(`${humanChoice} beats ${computerChoice}`);
-        console.log('You win!!');
-        humanScore++;
-    } else if ((humanChoice === 'rock' && computerChoice === 'paper') ||
-        (humanChoice === 'paper' && computerChoice === 'scissors') ||
-        (humanChoice === 'scissors' && computerChoice === 'rock')) {
-        console.log(`You chose ${humanChoice}, computer chose ${computerChoice}`);
-        console.log(`${computerChoice} beats ${humanChoice}`);
-        console.log('You lose');
-        computerScore++;
-    } else {
-        console.log(`You chose ${humanChoice}, computer chose ${computerChoice}`);
-        console.log('Draw');
-    }
-    console.log(`Your score: ${humanScore}, Computer score: ${computerScore}`);
-}
+// const playRound = (humanChoice, computerChoice) => {
+//     if ((humanChoice === 'rock' && computerChoice === 'scissors') ||
+//         (humanChoice === 'paper' && computerChoice === 'rock') ||
+//         (humanChoice === 'scissors' && computerChoice === 'paper')) {
+//         console.log(`You chose ${humanChoice}, computer chose ${computerChoice}`);
+//         console.log(`${humanChoice} beats ${computerChoice}`);
+//         console.log('You win!!');
+//         humanScore++;
+//     } else if ((humanChoice === 'rock' && computerChoice === 'paper') ||
+//         (humanChoice === 'paper' && computerChoice === 'scissors') ||
+//         (humanChoice === 'scissors' && computerChoice === 'rock')) {
+//         console.log(`You chose ${humanChoice}, computer chose ${computerChoice}`);
+//         console.log(`${computerChoice} beats ${humanChoice}`);
+//         console.log('You lose');
+//         computerScore++;
+//     } else {
+//         console.log(`You chose ${humanChoice}, computer chose ${computerChoice}`);
+//         console.log('Draw');
+//     }
+//     console.log(`Your score: ${humanScore}, Computer score: ${computerScore}`);
+// }
 
 // for (let i = 0; i < 5; i++) {
 //     let humanSelection = getHumanChoice();
@@ -45,21 +45,71 @@ const playRound = (humanChoice, computerChoice) => {
 const rock = document.querySelector('#rock');
 const paper = document.querySelector('#paper');
 const scissors = document.querySelector('#scissors');
+const displayScore = document.querySelector('.displayScore');
+const displayChoices = document.querySelector('.displayChoices');
+const displayRoundWinner = document.querySelector('.displayRoundWinner');
 
 rock.addEventListener('click', () => {
     let humanSelection = 'rock';
     let computerSelection = getComputerChoice();
-    playRound(humanSelection, computerSelection);
+    // playRound(humanSelection, computerSelection);
+    if (computerSelection === 'paper') {
+        displayChoices.textContent = `You chose rock, computer chose paper`
+        displayRoundWinner.textContent = 'Computer wins'
+        computerScore++;
+        displayScore.textContent = `Your score: ${humanScore}, Computer score: ${computerScore}`
+
+    } else if (computerSelection === 'scissors') {
+        displayChoices.textContent = `You chose rock, computer chose scissors`
+        displayRoundWinner.textContent = 'You win!'
+        humanScore++;
+        displayScore.textContent = `Your score: ${humanScore}, Computer score: ${computerScore}`
+
+    } else {
+        displayChoices.textContent = `You chose rock, computer chose rock`
+        displayRoundWinner.textContent = 'Draw'
+        displayScore.textContent = `Your score: ${humanScore}, Computer score: ${computerScore}`
+    }
 })
 
 paper.addEventListener('click', () => {
     let humanSelection = 'paper';
     let computerSelection = getComputerChoice();
-    playRound(humanSelection, computerSelection);
+    // playRound(humanSelection, computerSelection);
+    if (computerSelection === 'rock') {
+        displayChoices.textContent = `You chose paper, computer chose rock`
+        displayRoundWinner.textContent = 'You win!'
+        humanScore++;
+        displayScore.textContent = `Your score: ${humanScore}, Computer score: ${computerScore}`
+    } else if (computerSelection === 'scissors') {
+        displayChoices.textContent = `You chose paper, computer chose scissors`
+        displayRoundWinner.textContent = 'Computer wins'
+        computerScore++;
+        displayScore.textContent = `Your score: ${humanScore}, Computer score: ${computerScore}`
+    } else {
+        displayChoices.textContent = `You chose paper, computer chose paper`
+        displayRoundWinner.textContent = 'Draw'
+        displayScore.textContent = `Your score: ${humanScore}, Computer score: ${computerScore}`
+    }
 })
 
 scissors.addEventListener('click', () => {
     let humanSelection = 'scissors';
     let computerSelection = getComputerChoice();
-    playRound(humanSelection, computerSelection);
+    // playRound(humanSelection, computerSelection);
+    if (computerSelection === 'rock') {
+        displayChoices.textContent = `You chose scissors, computer chose rock`
+        displayRoundWinner.textContent = 'Computer wins'
+        computerScore++;
+        displayScore.textContent = `Your score: ${humanScore}, Computer score: ${computerScore}`
+    } else if (computerSelection === 'paper') {
+        displayChoices.textContent = `You chose scissors, computer chose paper`
+        displayRoundWinner.textContent = 'You win!'
+        humanScore++;
+        displayScore.textContent = `Your score: ${humanScore}, Computer score: ${computerScore}`
+    } else {
+        displayChoices.textContent = `You chose scissors, computer chose scissors`
+        displayRoundWinner.textContent = 'Draw'
+        displayScore.textContent = `Your score: ${humanScore}, Computer score: ${computerScore}`
+    }
 })
